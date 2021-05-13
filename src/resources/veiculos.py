67d7dc07b224b80d9220934c0fa3fd54affd1242
Jefferson Araujo <jefferson.silvaaraujo@hotmail.com>
2021-05-13 11:19:39 -0300

    ajuste em post

--- src/resources/veiculos.py
+++ src/resources/veiculos.py
@@ -79,9 +79,10 @@ class VeiculosList(Resource):
     @veiculos_ns.doc('Cria um novo veículo')
     def post(self):
         veiculo_json = request.get_json()   
-        print("JSON" + str(veiculo_json))
+       
         veiculo_json['created'] = datetime.utcnow
         veiculo_json['updated'] = datetime.utcnow
+        print("JSON" + str(veiculo_json))
         veiculo_data = veiculo_schema.load(veiculo_json)
         veiculo_data.save()
         return veiculo_schema.dump(veiculo_data), 201
